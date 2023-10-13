c8dd771d5d76547ca36d145786cca14125fa72ab
SinYoonHa <idc06012@naver.com>
2023-10-13 15:17:20 +0900

    백엔드 첫 커밋

---
+++ src/main/java/com/example/reservePlace/ReservePlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservePlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservePlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservePlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reservePlace/ReservePlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservePlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservePlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
