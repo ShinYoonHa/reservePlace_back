2ff7eb7f9f8815056f776dd3a4770de1bf05cabf
SinYoonHa <idc06012@naver.com>
2023-10-18 16:41:28 +0900

    회원정보수정 완료

--- src/main/java/com/example/reservePlace/controller/UserController.java
+++ src/main/java/com/example/reservePlace/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,4 +73,23 @@ public class UserController {
 		return ResponseEntity.badRequest().body(responseDTO);
 		}
 	}
+    /** 회원정보 수정 */
+    @PostMapping("/mypage")
+    public void edit_information(@RequestBody UserDTO userDTO) {
+
+            // 이전 유저 id 탐색
+            String before_userNum = userService.getUserEntity(userDTO.getUid()).getNum();
+
+            // 유저 id를 추가해서 Entity 생성
+            UserEntity update_user = UserEntity.builder()
+                    .num(before_userNum)
+                    .uid(userDTO.getUid())
+                    .username(userDTO.getUsername())
+                    .phone(userDTO.getPhone())
+                    .password(passwordEncoder.encode(userDTO.getPassword()))
+                    .build();
+
+            // 유저 정보 업데이트하기
+            userService.updateUserEntity(update_user);
+    }
 }
--- src/main/java/com/example/reservePlace/service/UserService.java
+++ src/main/java/com/example/reservePlace/service/UserService.java
@@ -1,9 +1,11 @@
 package com.example.reservePlace.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.reservePlace.dto.UserDTO;
 import com.example.reservePlace.model.UserEntity;
 import com.example.reservePlace.persistence.UserRepository;
 
@@ -13,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class UserService {
 	@Autowired
-	private UserRepository userRepository;
+	private UserRepository userRepository;	
 	
 	public UserEntity create(final UserEntity userEntity) {
 		if(userEntity == null || userEntity.getUid() == null) {
@@ -26,12 +28,19 @@ public class UserService {
 		}
 		return userRepository.save(userEntity);
 	}
-	public UserEntity getByCredentials(final String uid, final String password,
-			final PasswordEncoder encoder) {
+	public UserEntity getByCredentials(final String uid, final String password, final PasswordEncoder encoder) {
 		final UserEntity originalUser = userRepository.findByUid(uid);
 		if(originalUser != null && encoder.matches(password, originalUser.getPassword())) {
 			return originalUser;
 		}
 		return null;
 	}
+	public UserEntity getUserEntity(final String uid){
+        return userRepository.findByUid(uid);
+    }
+
+    public void updateUserEntity(final UserEntity userEntity){
+        userRepository.save(userEntity);
+    }
+
 }
